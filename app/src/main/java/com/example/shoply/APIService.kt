9255530061dbd3244c4ca51d36b6f143d7f7e23a
User_4 <elshodxushvaqtov07@gmail.com>
2023-10-17 08:46:51 +0500

    commit

---
+++ app/src/main/java/com/example/shoply/APIService.kt
@@ -0,0 +1,22 @@
+package com.example.shoply
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface APIService {
+    @GET("/products")
+    fun getAllProduct() : Call<List<ProductList>>
+
+    @GET("/products/{id}")
+    fun getProduct(@Path("id") id: Int): Call<Product>
+
+    @GET("products/search")
+    fun searchByName(@Query("q") name: String): Call<ProductList>
+
+    @POST("/auth/login")
+    fun login(@Body login: Login): Call<User>
+}
---
+++ app/src/main/java/com/example/shoply/Login.kt
@@ -0,0 +1,3 @@
+package com.example.shoply
+
+data class Login(var username:String, var password:String)
