c66483955e3e987a510e040edf7cc3ab6fa6e52a
User_3 <rmuhammadqodir58@gmail.com>
2023-10-18 15:29:00 +0500

    HomeFragment Commit4

--- app/src/main/java/com/example/shoply/MainFragment.kt
+++ app/src/main/java/com/example/shoply/MainFragment.kt
@@ -5,9 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentHostCallback
-import com.example.shoply.databinding.FragmentHomeBinding
-import com.google.android.material.navigation.NavigationBarItemView
+import androidx.fragment.app.FragmentManager
+import com.example.shoply.databinding.FragmentMainBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +15,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [MainFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class MainFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,9 +35,12 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       var binding = FragmentHomeBinding.inflate(layoutInflater)
-        parentFragmentManager.beginTransaction().replace(R.id.viewPager, ItemFragment())
-
+        var binding = FragmentMainBinding.inflate(layoutInflater)
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home ->
+            }
+        }
         return binding.root
     }
 
@@ -49,12 +51,12 @@ class HomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment MainFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            MainFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
