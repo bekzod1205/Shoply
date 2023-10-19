bc7337a98da1757244b6eb31b5e98012137c1d90
User_4 <elshodxushvaqtov07@gmail.com>
2023-10-19 11:25:59 +0500

    ogiloys_work

--- app/src/main/java/com/example/shoply/ItemSelected.kt
+++ app/src/main/java/com/example/shoply/ItemSelected.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager.widget.ViewPager
+import com.example.shoply.adapters.ViewPagerAdapterForPhotos
+import com.example.shoply.databinding.FragmentItemBinding
+import com.example.shoply.databinding.FragmentItemSelectedBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,6 +30,9 @@ class ItemSelected : Fragment() {
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
+
+
         }
     }
 
@@ -33,8 +40,10 @@ class ItemSelected : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_item_selected, container, false)
+
+
+        var binding = FragmentItemSelectedBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     companion object {
