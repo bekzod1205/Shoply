29701969dd40f47c3fd34ba6729dfa3f187552f2
User_3 <rmuhammadqodir58@gmail.com>
2023-10-19 10:58:34 +0500

    HomeFragment Commit5

---
+++ app/src/main/java/com/example/shoply/adapter/ProductsAdapter.kt
@@ -0,0 +1,9 @@
+package com.example.shoply.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+
+class ProductsAdapter:RecyclerView.Adapter<ProductsAdapter.MyHolder> {
+    class MyHolder():RecyclerView.ViewHolder{
+
+    }
+}
