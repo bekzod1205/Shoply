9a5df8ed8d009cac828cbfc9f635d0e171f546da
Shohrux <shoxruxsharipov1105@gmail.com>
2023-10-20 22:33:24 +0500

    commit

--- app/src/main/java/com/example/shoply/MainActivity.kt
+++ app/src/main/java/com/example/shoply/MainActivity.kt
@@ -2,28 +2,52 @@ package com.example.shoply
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.shoply.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var toggle:ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val drawerlayout: DrawerLayout = binding.drlayout
+        toggle = ActionBarDrawerToggle(this,drawerlayout,R.string.open,R.string.close)
+        drawerlayout.addDrawerListener(toggle)
+        toggle.syncState()
 
-        binding.apply {
-            toggle = ActionBarDrawerToggle(
-                this@MainActivity,
-                binding.drlayout,
-                R.string.open,
-                R.string.close
-            )
-            binding.drlayout.addDrawerListener(toggle)
-            toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.shopbycategories -> {
+                    Toast.makeText(applicationContext, "Shop by cat.", Toast.LENGTH_SHORT).show()
+                }
+                R.id.myorders -> {
+                    Toast.makeText(applicationContext, "My orders", Toast.LENGTH_SHORT).show()
+                }
+                R.id.favourites -> {
+                    Toast.makeText(applicationContext, "Favourites", Toast.LENGTH_SHORT).show()
+                }
+                R.id.faqs -> {
+                    Toast.makeText(applicationContext, "FAQs", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
         }
+
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
